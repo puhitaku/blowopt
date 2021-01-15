28f51dce54f4a00e98b6b90da5c167846ea014c0
Takumi Sueda <puhitaku@gmail.com>
2021-01-15 23:42:10 +0900

    Read volume and affect fan speed

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import json
 import utime
-from machine import Pin, PWM
+from machine import Pin, PWM, ADC
 
 import wifi
 
 
-def main():
+def main(conf):
+    adc = ADC(0)
     pwm = PWM(Pin(4), freq=10000)
-    pwm.duty(400)
+
     while True:
-        utime.sleep(999)
+        raw = adc.read()
+
+        raw -= conf['vol_range_min']
+        if raw < 0:
+            raw = 0
+
+        raw /= conf['vol_range_max']
+        if raw > 1.0:
+            raw = 1.0
+
+        raw = 1.0 - raw
+
+        print(1024 * raw)
+        pwm.duty(int(1024 * raw))
+        utime.sleep(0.1)
 
 
 def entrypoint():
@@ -19,7 +34,7 @@ def entrypoint():
     while True:
         wifi.up(conf['ssid'], conf['psk'])
         wifi.show_ifconfig()
-        main()
+        main(conf)
 
 
 entrypoint()
